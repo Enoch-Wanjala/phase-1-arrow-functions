403e1eb070e3e50216f80c5eae4c797f14490922
Enock <enochwanjala.97@gmail.com>
2023-10-05 20:31:27 +0300

    Completed arrow function assignement

---
+++ index.js
@@ -0,0 +1,11 @@
+// fundtion expression 
+function divide (dividend = 2000,divisor=100){
+ let result= dividend/divisor
+ return result
+}
+
+//Square function 
+const square =(num)=> num*num; 
+
+//Add function
+const add=(num1,num2)=>num1+num2;
